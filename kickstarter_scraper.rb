76d0c36c978841253e00444f92a36896d454aa1e
Elias Ventura <eliasventura23@gmail.com>
2018-11-06 20:38:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
-binding.pry
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
@@ -23,9 +22,7 @@ binding.pry
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project meta li span.location_name").text
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-
     }
-  end
   end
     projects
   end
